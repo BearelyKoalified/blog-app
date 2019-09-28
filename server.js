a47d3e81f6596c15c4719efd01e643a02856fdd4
Matt Abercrombie <bearelykoalified@gmail.com>
2019-09-28 08:30:51 -0400

    connect to database

--- server.js
+++ server.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+// Dev Config
+if(process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 app.get('/', (req, res) => res.send('API running'));
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server started on port ${port}`));
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+// DB Config
+const db = process.env.DATABASE_URL;
+
+// Connect to mongoDB
+mongoose
+  .connect(db, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+}).then(() => console.log('Mongoose Connected'))
+  .catch(err => console.log(err));
+
+
+
+
+
 
