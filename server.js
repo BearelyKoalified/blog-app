0dcf10b776130ae7e0aed14c81f44273540538cc
Matt Abercrombie <bearelykoalified@gmail.com>
2019-09-29 13:06:04 -0400

    temporarily removing bcrypt to fix intellisense issues - todo: add again when doing auth

--- server.js
+++ server.js
@@ -4,26 +4,17 @@ const config = require('config');
 
 const app = express();
 
-// Dev Config using .env file - undecided which approach I like more so leaving for now
-// if(process.env.NODE_ENV !== 'production') {
-//   require('dotenv').config()
-// }
-
 // Connect Database
 connectDB();
 
 app.get('/', (req, res) => res.send('API running'));
 
 // Define Routes
-// app.use('/api/users', require('./routes/api/users'));
 app.use('/api/users', require('./routes/api/users'));
-// app.use('/api/auth', require('./routes/api/auth'));
-// app.use('/api/profile', require('./routes/api/profile'));
-// app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-console.log('JWT_SECRET:' ,config.get('JWT_SECRET'));
-
