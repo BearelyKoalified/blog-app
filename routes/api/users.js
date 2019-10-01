97db5b8c36136bd25b857ca9a5b04018ddcaaf2a
Matt Abercrombie <bearelykoalified@gmail.com>
2019-10-01 07:26:33 -0400

    signing jwt and returning in registration req

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -57,18 +59,28 @@ router.post('/', [
     // save hash into password
     await user.save();
 
-    // todo: return json web token
-    res.send('User registered');
+    // JWT
+    const payload = {
+      user: {
+        id: user.id
+      }
+    };
+
+    jwt.sign(payload,
+      config.get('JWT_SECRET'),
+      { expiresIn: 360000 },
+      (err, token) => {
+      if (err) throw err;
+      res.json({ token });
+      }
+    );
+
+
 
   } catch(err) {
     console.error(err.message);
     res.status(500).send('Server Error');
   }
-
-
-
-
-  res.send('User route');
 });
 
 module.exports = router;
